0159cc265ded2428beb3ca722d8de3578591d80c
JG <grafjude@badinhs.org>
2016-10-06 09:56:44 -0400

    First page (table) with a segue to the next

--- Facultyandstaff/ViewController.swift
+++ Facultyandstaff/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:
+UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var facultystaffTable: UITableView!
+    
+    
+    var facultyStaff=["Teachers"]
+    var teachersDepartment=["English", "Math", "Science", "History", "Religion" ]
+   
+    override func viewDidLoad()
+        {
+            super.viewDidLoad()
+            facultystaffTable.dataSource=self
+            facultystaffTable.delegate=self
+        }
+    
+            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+                {
+                    return facultyStaff.count
+                }
+    
+            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+                {
+                    let cell=UITableViewCell()
+        
+                    print(indexPath.row)
+        
+                    cell.textLabel?.text=facultyStaff[indexPath.row]
+                    return cell
+                }
+            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+                {
+                    performSegue(withIdentifier: "teachersDepartment", sender: "Teachers")
+                }
+    
+    
+    override func didReceiveMemoryWarning()
+        {
+            super.didReceiveMemoryWarning()
+        }
 
 
 }
